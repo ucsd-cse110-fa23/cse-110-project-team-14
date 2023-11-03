02091e2f706d67eeb5483c6ca87541e42818b5f9
Marcos De La Torre <lookatmarcoosss@gmail.com>
2023-11-03 02:22:40 -0700

    removed gradle. everyone should be able to run and debug from menu now.

--- src/InitialUi.java
+++ src/InitialUi.java
@@ -1,110 +1,104 @@
 package src;
-
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javax.swing.border.Border;
 
-class Header extends HBox {
-    Header() {
-        this.setPrefSize(1000, 60);
-        this.setStyle("-fx-background-color: #F5DEB3; -fx-border-radius: 15 15 0 0; -fx-background-radius: 15 15 0 0;");
-        
-        Text titleText = new Text("PantryPal: The best Recipe manager");
-        titleText.setFill(Color.web("#8B4513"));  // Saddle Brown
+class Header extends HBox
+{
+    Header()
+    {
+        this.setPrefSize(1000, 60); // Size of the header
+        this.setStyle("-fx-background-color: #F9E69A;");
+
+        Text titleText = new Text("PantryPal----The best Recipe manager"); // Write what ever you want
         titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 26;");
         this.getChildren().add(titleText);
         this.setAlignment(Pos.CENTER);
-        
-        // Add shadow for depth
-        DropShadow ds = new DropShadow();
-        ds.setOffsetY(3.0);
-        ds.setColor(Color.color(0.4, 0.4, 0.4));
-        this.setEffect(ds);
     }
 }
+class Footer extends HBox//TODO: place for Button
+{
+    //TODO: create private Button variable here:
+    Footer()
+    {
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #F9E69A;");
+        this.setSpacing(15);
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
+        //TODO: add Button here:
 
-class Footer extends HBox {
-    private Button addButton;
 
-    Footer() {
-        this.setPrefSize(1000, 60);
-        this.setStyle("-fx-background-color: #F5DEB3; -fx-border-radius: 0 0 15 15; -fx-background-radius: 0 0 15 15;");
-
-        addButton = new Button("Create Recipe");
-        addButton.setStyle("-fx-background-color: #FFEBD7; -fx-text-fill: #8B4513; -fx-border-color: #8B4513; -fx-border-radius: 20; -fx-background-radius: 20; -fx-padding: 5 15 5 15;");
-
-        this.getChildren().addAll(addButton);
-        this.setAlignment(Pos.CENTER);
-
-        // Add shadow for depth
-        DropShadow ds = new DropShadow();
-        ds.setOffsetY(-3.0);
-        ds.setColor(Color.color(0.4, 0.4, 0.4));
-        this.setEffect(ds);
+        //this.getChildren().addAll(); //TODO: adding buttons to footer
+        this.setAlignment(Pos.CENTER); // aligning the buttons to center
     }
-
-    public Button getAddButton() {
-        return addButton;
-    }
+    //TODO: getButton methods:
 }
+class Appframe extends BorderPane
+{
 
+    //TODO: add the class we need, like button, Header, Footer
 
-
-class Appframe extends BorderPane {
+    ////Header:
     private Header header;
+    ////Footer---for buttons
     private Footer footer;
-    private Button addButton;
+    //TODO: add the RecipeList here:
 
-    Appframe() {
+    //TODO: add Button here:
+
+
+    Appframe()
+    {
+        // Initialise the header Object
         header = new Header();
+
+        //TODO: Create a RecipeList Object to hold the tasks
+
+
+        // Initialise the Footer Object
         footer = new Footer();
 
-        VBox mainContent = new VBox();
-        mainContent.setSpacing(15);
-        mainContent.setAlignment(Pos.CENTER);
-
+        // Add header to the top of the BorderPane
         this.setTop(header);
-        this.setCenter(mainContent);
+        //TODO: Add scroller to the centre of the BorderPane
+
+        // Add footer to the bottom of the BorderPane
         this.setBottom(footer);
 
-        this.addButton = footer.getAddButton();
+        this.setStyle("-fx-background-color: #F9E69A;");//change to what ever color you like
+        //TODO:Initialise Button Variables through the getters in Footer
 
-        this.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, #FFE4B5, #FFDEAD, #FFE4B5, #FFDEAD); " +
-                      "-fx-border-color: #DEB887; " +
-                      "-fx-border-width: 10; " +
-                      "-fx-border-radius: 15; " +
-                      "-fx-background-radius: 15;");
+        //TODO: add listener
 
-        addListeners(); //define later
-    }
-
-        public void addListeners() {
-            addButton.setOnAction(e -> {
-                //action of the button should code (chatGPT CALL)
-                System.out.println("BUTTON WORKS :)");
-            });
-        }
 
     }
-
+}
 
 public class InitialUi extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // Setting the Layout of the Window- Should contain a Header, Footer and the RecipeList
         Appframe root = new Appframe();
+
+        // Set the title of the app
         primaryStage.setTitle("PantryPal");
+        // Create scene of mentioned size with the border pane
         primaryStage.setScene(new Scene(root, 600, 900));
+        // Make window non-resizable
         primaryStage.setResizable(false);
+        // Show the app
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
