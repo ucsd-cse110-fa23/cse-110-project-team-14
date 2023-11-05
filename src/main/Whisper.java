7f09588317de709082791853f7206fff609f8e15
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2023-11-04 18:40:42 -0700

    need to get rid of main in Whisper.java and convert to methods

--- src/main/Whisper.java
+++ src/main/Whisper.java
@@ -1,9 +1,14 @@
 package src.main;
 
+import java.io.*;
+import java.net.*;
+import org.json.*;
+
 public class Whisper {
     private static final String API_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions";
     private static final String TOKEN = "";
     private static final String MODEL = "whisper-1";
+    private static final String FILE_PATH = ""; // path to audio
 
     private static void writeParameterToOutputStream(
             OutputStream outputStream,
@@ -74,7 +79,6 @@ public class Whisper {
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final String FILE_PATH = args[0];
 
         // Create file object from file path
         File file = new File(FILE_PATH);
