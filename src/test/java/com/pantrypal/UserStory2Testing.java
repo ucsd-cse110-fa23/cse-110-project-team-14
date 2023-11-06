f81ae272be08f5405e56360316843de590ccd7b7
Jivan132 <jesusiam3002@gmail.com>
2023-11-05 19:29:17 -0800

    Fixing main

---
+++ src/test/java/com/pantrypal/UserStory2Testing.java
@@ -0,0 +1,11 @@
+package com.pantrypal;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+public class UserStory2Testing {
+    
+}
