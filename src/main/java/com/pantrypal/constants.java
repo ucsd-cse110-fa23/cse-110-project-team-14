3d25ba7770c6aaaeadb918b2005719ba6fbcae34
Jivan132 <jesusiam3002@gmail.com>
2023-11-24 11:34:53 -0800

    Change size

--- src/main/java/com/pantrypal/constants.java
+++ src/main/java/com/pantrypal/constants.java
@@ -1,6 +1,6 @@
 package com.pantrypal;
 
 public class constants {
-    public final static int width = 900;
-    public final static int height = 900;
+    public final static int width = 600;
+    public final static int height = 600;
 }
