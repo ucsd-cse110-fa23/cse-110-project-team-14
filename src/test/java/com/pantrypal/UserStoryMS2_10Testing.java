ea7c73774cf4f1843401a6932d87c962f1f14a5f
Jivan132 <jesusiam3002@gmail.com>
2023-11-28 18:45:46 -0800

    Issue #100 Added BDD Test

--- src/test/java/com/pantrypal/UserStoryMS2_10Testing.java
+++ src/test/java/com/pantrypal/UserStoryMS2_10Testing.java
@@ -3,6 +3,8 @@ package com.pantrypal;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class UserStoryMS2_10Testing {
@@ -26,9 +28,10 @@ public class UserStoryMS2_10Testing {
         }
         Sort sort = new Sort();
         recipes = sort.newToOldSort(recipes);
-
+        int j = 9;
         for (int i = 0; i < 10; i++) {
-            assertEquals("TInstr" + (i), recipes.get(i).getRecipeInstructions());
+            assertEquals("TInstr" + (j), recipes.get(i).getRecipeInstructions());
+            j--;
         }
     }
 
@@ -50,20 +53,17 @@ public class UserStoryMS2_10Testing {
         }
         Sort sort = new Sort();
         recipes = sort.oldToNewSort(recipes);
-
+        int j = 9;
         for (int i = 0; i < 10; i++) {
-            assertEquals("TInstr" + (i), recipes.get(i).getRecipeInstructions());
+            assertEquals("TInstr" + (j), recipes.get(i).getRecipeInstructions());
+            j--;
         }
     }
-
     /**
-     * Test case to verify the functionality of sorting recipes in alphabetical order.
-     * It creates a list of recipes with titles starting from 'Z' and ending at 'R',
-     * and then sorts them in ascending order using the aToZSort() method from the Sort class.
-     * Finally, it asserts that the sorted list is in the expected order.
+     * Test case to verify the sorting of recipes in descending order by title (Z to A).
      */
     @Test
-    public void sortingAtoZ(){
+    public void sortingZtoA(){
         ArrayList<Recipe> recipes = new ArrayList<Recipe>();
         char title = 90;
         for (int i = 0; i < 10; i++) {
@@ -77,20 +77,21 @@ public class UserStoryMS2_10Testing {
         }
         Sort sort = new Sort();
         recipes = sort.aToZSort(recipes);
-        title++;
-        int counter = 9;
+        title = 90;
         for (int i = 0; i < 10; i++) {
-            assertEquals(title + "TR" + counter, recipes.get(i).getRecipeTitle());
-            counter--;
-            title++;
+            assertEquals(title + "TR" + i, recipes.get(i).getRecipeTitle());
+            title--;
         }
     }
 
     /**
-     * Test case to verify the sorting of recipes in descending order by title (Z to A).
+     * Test case to verify the functionality of sorting recipes in alphabetical order.
+     * It creates a list of recipes with titles starting from 'Z' and ending at 'R',
+     * and then sorts them in ascending order using the aToZSort() method from the Sort class.
+     * Finally, it asserts that the sorted list is in the expected order.
      */
     @Test
-    public void sortingZtoA(){
+    public void sortingAtoZ(){
         ArrayList<Recipe> recipes = new ArrayList<Recipe>();
         char title = 65;
         for (int i = 0; i < 10; i++) {
@@ -104,12 +105,75 @@ public class UserStoryMS2_10Testing {
         }
         Sort sort = new Sort();
         recipes = sort.zToASort(recipes);
-        title--;
-        int counter = 9;
+        title = 65;
         for (int i = 0; i < 10; i++) {
-            assertEquals(title + "TR" + counter, recipes.get(i).getRecipeTitle());
-            counter--;
-            title--;
+            assertEquals(title + "TR" + i, recipes.get(i).getRecipeTitle());
+            title++;
         }
     }
+
+    /*
+     * Tests that integrates this with ChatGPT and Whisper
+     */
+    @Test
+    public void sortingAtoZAndGPT(){
+        MockWhisper whisper = new MockWhisper();
+        TextToRecipe textToRecipe = new TextToRecipe(whisper.getResponse(6), whisper.getResponse(2), new Recipe(), new MockChatGPT());
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        try {
+            textToRecipe.createRecipeObj();
+            assertEquals("Egg Tortilla Breakfast Sandwich", textToRecipe.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        MockWhisper whisper2 = new MockWhisper();
+        TextToRecipe textToRecipe2 = new TextToRecipe(whisper2.getResponse(1), "dinner", new Recipe(), new MockChatGPT());
+        try {
+            textToRecipe2.createRecipeObj();
+            assertEquals("Baked Cinnamon Apple Crisp", textToRecipe2.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe2.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        MockWhisper whisper3 = new MockWhisper();
+
+        String prompt = "Make me a dinner recipe using only these ingredients: chicken, rice. \n" +
+        "Please give me the recipe following this format:\n" +
+        "Recipe title\n" +
+        "Ingredients:\n" +
+        "Instructions:\n";
+        TextToRecipe textToRecipe3 = new TextToRecipe(whisper.getResponse(0), "dinner", new Recipe(), new MockChatGPT());
+        try {
+            textToRecipe3.createRecipeObj();
+            assertEquals("Chicken and Rice Casserole", textToRecipe3.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe3.getRecipe());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        Sort sort = new Sort();
+        recipes = sort.aToZSort(recipes);
+        assertEquals("Baked Cinnamon Apple Crisp", recipes.get(2).getRecipeTitle());
+        assertEquals("Chicken and Rice Casserole", recipes.get(1).getRecipeTitle());
+        assertEquals("Egg Tortilla Breakfast Sandwich", recipes.get(0).getRecipeTitle());
+        
+    }
 }
+
+
