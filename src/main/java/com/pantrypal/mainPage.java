69a4db547be80a4e41f4d86bd11c966de481a404
Jivan132 <jesusiam3002@gmail.com>
2023-11-28 18:48:53 -0800

    Issue #99 Added implementation for dropdown action

--- src/main/java/com/pantrypal/mainPage.java
+++ src/main/java/com/pantrypal/mainPage.java
@@ -1,7 +1,11 @@
 package com.pantrypal;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -18,6 +22,15 @@ class mainPage extends Page {
         // IntializeRecipeList.uploadRecipes();
         DatabaseOPS db = new DatabaseOPS("recipes");
         Globals.recipes = db.initializeRecipesToList(); //<-- this will return the arraylist
+        if(Globals.sortingState == Globals.SortingState.NEWTOOLD) {
+            Globals.recipes = Sort.newToOldSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.OLDTONEW) {
+            Globals.recipes = Sort.oldToNewSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.ATOZ) {
+            Globals.recipes = Sort.aToZSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.ZTOA) {
+            Globals.recipes = Sort.zToASort(Globals.recipes);
+        }
         for (int i = Globals.recipes.size() - 1; i >= 0; i--) {
             Recipe recipe = Globals.recipes.get(i);
             RecipeTitleView recipeTitleView = new RecipeTitleView(recipe); //<-- This should be UI
@@ -48,6 +61,7 @@ class mainPage extends Page {
         
     }
 
+    //Change to use a helper method
     public void addListeners() {
         addButton.setOnAction(e -> {
             // swap to record page
@@ -69,6 +83,56 @@ class mainPage extends Page {
 
         paneHeader = new paneHeader();
         paneHeader.setTitleInMiddle(new Text("PantryPal: The best Recipe manager"));
+
+        ComboBox<String> comboBox = new ComboBox<>();
+
+        // Add items to the ComboBox
+        ObservableList<String> items = FXCollections.observableArrayList(
+                "Newest to Oldest",
+                "Oldest to Newest",
+                "A to Z",
+                "Z to A"
+        );
+
+        comboBox.setItems(items);
+
+        comboBox.setOnAction(e -> {
+                // TODO:  Put into a helper method for refactoring purposes
+                
+                // Get the selected item
+                String selectedOption = comboBox.getValue();
+                // do a switch case to check between "Newest to Oldest", "Oldest to Newest", "A to Z", "Z to A"
+                switch(selectedOption) {
+                    case "Newest to Oldest":
+                        // sort by newest to oldest
+                        //NEWTOOLD
+                        Globals.sortingState = Globals.SortingState.NEWTOOLD;
+                        update();
+                        break;
+                    case "Oldest to Newest":
+                        // sort by oldest to newest
+                        // OLDTONEW
+                        Globals.sortingState = Globals.SortingState.OLDTONEW;
+                        update();
+                        break;
+                    case "A to Z":
+                        // sort by A to Z
+                        // ATOZ
+                        Globals.sortingState = Globals.SortingState.ATOZ;
+                        update();
+                        break;
+                    case "Z to A":
+                        // sort by Z to A
+                        // ZTOA
+                        Globals.sortingState = Globals.SortingState.ZTOA;
+                        update();
+                        break;
+                }
+                // Add your functionality here
+                System.out.println("Selected: " + selectedOption);
+        });
+
+        VBox vBox = new VBox(comboBox);
         
         ScrollPane scroll = new ScrollPane(RecipeTitleListView.getInstance());
         scroll.setPrefSize(1000, 1000);
@@ -82,6 +146,7 @@ class mainPage extends Page {
         mainContent.minWidth(1000);
         mainContent.setSpacing(15);
         mainContent.setAlignment(Pos.CENTER);
+        mainContent.getChildren().add(vBox); // may have to redo
         mainContent.getChildren().add(scroll);
         
         this.borderPane.setTop(paneHeader);
@@ -103,5 +168,33 @@ class mainPage extends Page {
                 "-fx-border-radius: 15; " +
                 "-fx-background-radius: 15;");
         addListeners();
+    }
+
+    public void update(){
+        RecipeTitleListView.getInstance().getChildren().clear();
+        DatabaseOPS db = new DatabaseOPS("recipes");
+        Globals.recipes = db.initializeRecipesToList(); //<-- this will return the arraylist
+        if(Globals.sortingState == Globals.SortingState.NEWTOOLD) {
+            Globals.recipes = Sort.newToOldSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.OLDTONEW) {
+            Globals.recipes = Sort.oldToNewSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.ATOZ) {
+            Globals.recipes = Sort.aToZSort(Globals.recipes);
+        } else if(Globals.sortingState == Globals.SortingState.ZTOA) {
+            Globals.recipes = Sort.zToASort(Globals.recipes);
+        }
+        RecipeTitleListView.getInstance().getChildren().clear();
+        for (int i = Globals.recipes.size() - 1; i >= 0; i--) {
+            Recipe recipe = Globals.recipes.get(i);
+            RecipeTitleView recipeTitleView = new RecipeTitleView(recipe); //<-- This should be UI
+            SeeRecipePage SRP = new SeeRecipePage(constants.width, constants.height); //<-- This should be UI
+            SRP.setRecipe(recipe);
+            recipeTitleView.getRecipeTitleButton().setOnAction(e1 -> {
+                StageController stg = StageController.getInstance();
+                stg.registerPage(recipe.getRecipeTitle(), SRP);
+                stg.changeTo(recipe.getRecipeTitle());
+            });
+            RecipeTitleListView.getInstance().getChildren().add(recipeTitleView);
+        }
     }
 }
