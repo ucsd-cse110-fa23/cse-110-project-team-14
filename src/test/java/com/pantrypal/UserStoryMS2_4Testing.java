e35004dfb2bee337a044f417de4fb72475754c5c
Jivan132 <jesusiam3002@gmail.com>
2023-12-05 12:32:13 -0800

    Issue #83 Added tests

--- src/test/java/com/pantrypal/UserStoryMS2_4Testing.java
+++ src/test/java/com/pantrypal/UserStoryMS2_4Testing.java
@@ -1,40 +1,40 @@
-// package com.pantrypal;
+package com.pantrypal;
 
-// import org.junit.jupiter.api.Test;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import java.io.IOException;
-// import java.net.URISyntaxException;
-
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 
-// public class UserStoryMS2_4Testing {
 
-//     //Tests if creation works for a new user
-//     @Test
-//     public void testCreateAccount() throws IOException, URISyntaxException {
-//         DatabaseOPS db = new DatabaseOPS("users_test");
-//         db.deleteAllUsers();
-//         CreateAccount ca = new CreateAccount();
-//         ca.setCollectionName("users_test");
-//         boolean result = ca.createAccount("test", "test");
-//         assertEquals(true, result);
 
-//         result = ca.createAccount("test", "test");
-//         assertEquals(false, result);
-//     }
+public class UserStoryMS2_4Testing {
 
-//     //Tests if creation works for an existing user
-//     @Test
-//     public void testCreateAccount2() throws IOException, URISyntaxException {
-//         DatabaseOPS db = new DatabaseOPS("users_test");
-//         db.deleteAllUsers();
-//         CreateAccount ca = new CreateAccount();
-//         ca.setCollectionName("users_test");
-//         boolean result = ca.createAccount("test", "test");
-//         assertEquals(true, result);
+    //Tests if creation works for a new user
+    @Test
+    public void testLogin() throws IOException, URISyntaxException {
+        DatabaseOPS db = new DatabaseOPS("users_test");
+        db.deleteAllUsers();
+        Login lo = new Login();
+        lo.setCollectionName("users_test");
+        boolean result = lo.checkCredentials("test", "test");
+        assertEquals(false, result);
+    }
 
-//         result = ca.createAccount("test", "test");
-//         assertEquals(false, result);
-//     }
-// }
+    //Tests if creation works for an existing user
+    @Test
+    public void testLoginAndCreate() throws IOException, URISyntaxException {
+        DatabaseOPS db = new DatabaseOPS("users_test");
+        db.deleteAllUsers();
+        Login lo = new Login();
+        lo.setCollectionName("users_test");
+        boolean result = lo.checkCredentials("test", "test");
+        assertEquals(false, result);
+
+        CreateAccount ca = new CreateAccount();
+        ca.setCollectionName("users_test");
+        result = ca.createAccount("test", "test");
+        result = lo.checkCredentials("test", "test");
+        assertEquals(true, result);
+    }
+}
