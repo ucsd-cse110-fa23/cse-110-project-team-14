7b4d16ce3ac76693f554b360a3b30328fbe9dc35
Marcos De La Torre <lookatmarcoosss@gmail.com>
2023-12-04 17:52:55 -0800

    Issue#101-104 Server Status

--- src/main/java/com/pantrypal/Main.java
+++ src/main/java/com/pantrypal/Main.java
@@ -1,25 +1,67 @@
 package com.pantrypal;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 public class Main extends Application {
     LoginPage root = new LoginPage(constants.width, constants.height);
     // mainPage root = new mainPage(constants.width, constants.height);
+    public static StageController stgControl = StageController.getInstance();
+    public static boolean wasError;
+    private static String lastSuccessfulPage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("PantryPal");
         primaryStage.setResizable(false);
         primaryStage.setScene(root.getScene());
-        StageController stgControl = StageController.getInstance();
         stgControl.init(primaryStage);
         stgControl.registerPage("LoginPage", root);
         stgControl.registerPage("RegisterPage", new RegisterPage(constants.width,constants.height));
         stgControl.registerPage("mainPage", new mainPage(constants.width,constants.height));
         stgControl.registerPage("MealTypePage",new MealTypePage(constants.width,constants.height));
-        //TODO: we need load the all saved recipe from database
+        stgControl.registerPage("ErrorPage",new ErrorPage(constants.width,constants.height));
 
         stgControl.registerPage("RecordPage", new RecordPage(constants.width, constants.height));
         primaryStage.show();
+        // Schedule a task to check the server status every 5 seconds
+        
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(() -> {
+            if(wasError){
+                Platform.runLater(() -> stgControl.changeTo(lastSuccessfulPage));
+            }
+            if (!isServerRunning()) {
+                Platform.runLater(() -> stgControl.changeTo("ErrorPage"));
+                wasError = true;
+            } 
+            else{
+                lastSuccessfulPage = stgControl.getCurrentPage();
+                wasError = false;
+            }
+        }, 0, 5, TimeUnit.SECONDS); 
+    }
+
+    private static boolean isServerRunning() {
+        String SERVER_HOSTNAME = "localhost";
+        String SERVER_PORT = "8100";
+    
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://" + SERVER_HOSTNAME + ":" + SERVER_PORT + "/status").openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Exception occurred: " + e.getMessage());
+            return false;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/pantrypal/Server.java
+++ src/main/java/com/pantrypal/Server.java
@@ -21,19 +21,25 @@ import com.sun.net.httpserver.HttpServer;
 public class Server {
     private static final int SERVER_PORT = 8100;
     private static final String SERVER_HOSTNAME = "localhost";
+    private static boolean isServerRunning = false;
 
     public static void main(String[] args) throws IOException {
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
-        Map<String, byte[]> data = new HashMap<>();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(SERVER_HOSTNAME, SERVER_PORT), 0);
         server.createContext("/share", new URLShareHandler());
+        server.createContext("/status", new StatusHandler());
         server.setExecutor(threadPoolExecutor);
         server.start();
+        isServerRunning = true;
     
 
         System.out.println("Server started on port " + SERVER_PORT);
         
+    }
+
+    public static boolean isServerRunning() {
+        return isServerRunning;
     }
     
 
