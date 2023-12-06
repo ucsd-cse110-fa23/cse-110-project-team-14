624ee93ecc673a7e858dec5234c171f21096bc97
Jivan132 <jesusiam3002@gmail.com>
2023-12-05 21:33:55 -0800

    End to end test MS2-US9

--- src/test/java/com/pantrypal/UserStoryMS2_9Testing.java
+++ src/test/java/com/pantrypal/UserStoryMS2_9Testing.java
@@ -3,9 +3,12 @@ package com.pantrypal;
 import org.junit.jupiter.api.Test;
 
 import com.pantrypal.model.Recipe;
+import com.pantrypal.model.TextToRecipe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class UserStoryMS2_9Testing {
@@ -145,5 +148,111 @@ public class UserStoryMS2_9Testing {
             assertEquals(title + "TR" + i, recipes.get(i).getRecipeTitle());
             title++;
         }
+    }
+
+    @Test
+    public void testMS2_9BDD(){
+        MockWhisper whisper = new MockWhisper();
+        TextToRecipe textToRecipe = new TextToRecipe(whisper.getResponse(6), whisper.getResponse(2), new Recipe(), new MockChatGPT(), new MockImageCreation());
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        try {
+            textToRecipe.createRecipeObj();
+            assertEquals("Egg Tortilla Breakfast Sandwich", textToRecipe.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        MockWhisper whisper2 = new MockWhisper();
+        TextToRecipe textToRecipe2 = new TextToRecipe(whisper2.getResponse(1), "dinner", new Recipe(), new MockChatGPT(), new MockImageCreation());
+        try {
+            textToRecipe2.createRecipeObj();
+            assertEquals("Baked Cinnamon Apple Crisp", textToRecipe2.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe2.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        TextToRecipe textToRecipe3 = new TextToRecipe(whisper.getResponse(0), "dinner", new Recipe(), new MockChatGPT(), new MockImageCreation());
+        try {
+            textToRecipe3.createRecipeObj();
+            assertEquals("Chicken and Rice Casserole", textToRecipe3.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe3.getRecipe());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(3, recipes.size());
+        recipes = Filters.filterByDinner(recipes);
+        assertEquals(2, recipes.size());
+
+        for(int i = 0; i < recipes.size(); i++){
+            assertEquals("Dinner", recipes.get(i).getMealType());
+        }
+    }
+
+    @Test
+    public void testMS2_9BDD2(){
+        MockWhisper whisper = new MockWhisper();
+        TextToRecipe textToRecipe = new TextToRecipe(whisper.getResponse(6), whisper.getResponse(2), new Recipe(), new MockChatGPT(), new MockImageCreation());
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        try {
+            textToRecipe.createRecipeObj();
+            assertEquals("Egg Tortilla Breakfast Sandwich", textToRecipe.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        MockWhisper whisper2 = new MockWhisper();
+        TextToRecipe textToRecipe2 = new TextToRecipe(whisper2.getResponse(1), "dinner", new Recipe(), new MockChatGPT(), new MockImageCreation());
+        try {
+            textToRecipe2.createRecipeObj();
+            assertEquals("Baked Cinnamon Apple Crisp", textToRecipe2.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe2.getRecipe());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        TextToRecipe textToRecipe3 = new TextToRecipe(whisper.getResponse(0), "dinner", new Recipe(), new MockChatGPT(), new MockImageCreation());
+        try {
+            textToRecipe3.createRecipeObj();
+            assertEquals("Chicken and Rice Casserole", textToRecipe3.getRecipe().getRecipeTitle());
+            recipes.add(textToRecipe3.getRecipe());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(3, recipes.size());
+        recipes = Filters.filterByLunch(recipes);
+        assertEquals(0, recipes.size());
     }
 }
